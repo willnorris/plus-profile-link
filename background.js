bb63a33fe13a36ee6ec83a0db7cf93057e546db9
Will Norris <will@willnorris.com>
2012-01-20 18:02:29 -0800

    ensure we only open the profile for current tab

--- background.js
+++ background.js
@@ -1,14 +1,32 @@
-function onRequest(request, sender, sendResponse) {
+/**
+ * Dictionary of detected profile URLs keyed off of tabId.
+ */
+var profileUrls = {}
+
+
+/**
+ * Listen for the content script to send a message to the background page.
+ */
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  profileUrls[sender.tab.id] = request.href;
   chrome.pageAction.show(sender.tab.id);
 
-  chrome.pageAction.onClicked.addListener(function(tab) {
-    chrome.tabs.create({'url': request.href});
-  });
-
   sendResponse({});
-}
+});
 
-// Listen for the content script to send a message to the background page.
-chrome.extension.onRequest.addListener(onRequest);
 
+/**
+ * Open profile URL when the page action icon is clicked.
+ */
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.create({'url': profileUrls[tab.id]});
+});
+
+
+/**
+ * Cleanup when tabs are closed.
+ */
+chrome.tabs.onRemoved.addListener(function(tabId) {
+  delete profileUrls[tabId];
+});
 
